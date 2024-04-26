5cbae96fbb7b13781701b23e3827b4852083685b
zeynabsadat1380 <167699936+zeynabsadat1380@users.noreply.github.com>
2024-04-26 14:00:09 +0330

    Add files via upload

---
+++ zeshiri5.ino
@@ -0,0 +1,16 @@
+float voltage;
+int ledpin=10;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  float voltage=analogRead(A1);
+  float voltage1=voltage/1023*5;
+  float voltage2=map(voltage,0,1023,0,255);
+  Serial.print("voltage=");
+  Serial.println(voltage1);
+//  delay(1000);
+  analogWrite(10,voltage2);
+   
+}
